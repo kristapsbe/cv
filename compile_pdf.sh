dcca53f214f45a1c83c2d4f0b6d308ad6a39199e
Kristaps <kbebris+git@gmail.com>
2024-11-23 14:04:08 +0200

    moved contact info to separate file

--- compile_pdf.sh
+++ compile_pdf.sh
@@ -1,2 +1,7 @@
 rm pdfs/*
+
+if [ ! -e "contacts.tex" ]; then
+    cp contacts.example.tex contacts.tex
+fi
+
 pdflatex -output-director=pdfs -jobname=cv_kristaps_bebris_$(date +"%Y%m%d_%H%M") cv.tex
